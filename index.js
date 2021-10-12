5e2df8eed40f24dba6f8fc3b73b26911df67c732
Blurpyx <tommytytgat.tt@gmail.com>
2021-10-12 09:55:26 +0200

    tabel invullen met json data

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 fetch("./data.json")
-  .then(res => res.json())
-  .then(data => {
-    try {
-      data.forEach(element => {
-        var table = document.querySelector("#json-table");
-        var tr = document.createElement("tr");
-        var td_voornaam = document.createElement("td");
-        var td_naam = document.createElement("td");
-        var td_bericht = document.createElement("td");
-  
-        td_voornaam.innerHTML = element.Voornaam;
-        tr.appendChild(td_voornaam);
-        td_naam.innerHTML = element.Naam;
-        tr.appendChild(td_naam);
-        td_bericht.innerHTML = element.Bericht;
-        tr.appendChild(td_bericht);
-        table.appendChild(tr);
-      });
-    }
-    catch (err) {
-      console.log(err.message);
-    }
-  });
+.then(res => res.json())
+.then(data => {
+  try {
+    data.forEach(element => {
+      var table = document.querySelector("#json-table");
+      var tr = document.createElement("tr");
+      var td_voornaam = document.createElement("td");
+      var td_naam = document.createElement("td");
+      var td_bericht = document.createElement("td");
+
+      td_voornaam.innerHTML = element.Voornaam;
+      tr.appendChild(td_voornaam);
+      td_naam.innerHTML = element.Naam;
+      tr.appendChild(td_naam);
+      td_bericht.innerHTML = element.Bericht;
+      tr.appendChild(td_bericht);
+      table.appendChild(tr);
+    });
+  }
+  catch (err) {
+    console.log(err.message);
+  }
+});
